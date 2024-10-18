001aca3ce86067a93976ad461fe1d65d5d13e4ac
Danila <kovallll.e@gmail.com>
2024-10-18 22:29:56 +0300

    fix: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import removeTestIdAttribute from 'rollup-plugin-jsx-remove-attributes'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
+import removeAttribute from '@castlenine/vite-remove-attribute'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -10,9 +10,9 @@ export default defineConfig({
     plugins: [
         react(),
         viteTsconfigPaths(),
-        removeTestIdAttribute({
+        removeAttribute({
+            extensions: ['tsx'],
             attributes: ['data-cy'],
-            usage: 'vite',
         }),
         svgr(),
     ],
