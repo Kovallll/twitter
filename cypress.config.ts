1be61bf5b82d2532ca9d9245a1a1640f21b4377b
Danila <kovallll.e@gmail.com>
2024-10-18 13:50:58 +0300

    fix: fix ci cypress, add order run tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-    e2e: { baseUrl: 'http://localhost:5173' },
+    e2e: {
+        baseUrl: 'http://localhost:5173',
+        specPattern: [
+            'signUp.cy.ts',
+            'signUpCredentials.cy.ts',
+            'login.cy.ts',
+            'profile.cy.ts',
+            'home.cy.ts',
+        ],
+    },
     viewportWidth: 1440,
     viewportHeight: 800,
 })
